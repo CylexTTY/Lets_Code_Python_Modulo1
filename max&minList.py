4d7e341c304709307ea6d6c66dc40b1c33556e84
Lucas Felix <85033278+CylexTTY@users.noreply.github.com>
2021-07-29 16:22:01 -0300

    Maior e menor valor de uma lista de listas
    2 funções para saber o maior e menor valor de uma lista (podendo ser lista de listas) com a possibilidade de mostrar quantas vezes esse numero aparece na lista.
    Tudo isso sem usar o max() e min();
    Usando list comprehension e função recursiva.

---
+++ max&minList.py
@@ -0,0 +1,28 @@
+def maxList(lista, count=0):
+    num_list = [maxList(el)
+                if isinstance(el, list) else el
+                for el in lista]
+    maior = [n
+             for n in num_list
+             if not False in [True
+                              if n >= num else False
+                              for num in num_list]]
+    if count == 0:
+        return maior[0]
+    elif count == 1:
+        return maior[0], len(maior)
+
+
+def minList(lista, count=0):
+    num_list = [minList(el)
+                if isinstance(el, list) else el
+                for el in lista]
+    menor = [n
+             for n in num_list
+             if not False in [True
+                              if n <= num else False
+                              for num in num_list]]
+    if count == 0:
+        return menor[0]
+    elif count == 1:
+        return menor[0], len(menor)
