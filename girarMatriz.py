db755d60f1bdcf89b4124feae8235c61b2610fbf
Lucas Felix <85033278+CylexTTY@users.noreply.github.com>
2021-08-15 12:42:56 -0300

    Girar matriz 90º graus sentido horário 'n' vezes.
    Simples programa para girar matriz 90º graus sentido horário se adaptando com tamanho da matriz solicitada pelo usuário.
    Podendo ser rotacionada 'n' vezes.

---
+++ girarMatriz.py
@@ -0,0 +1,59 @@
+'''
+Objetivo:
+Girar matriza noventa graus, sentido horário.
+
+matriz original:
+[
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+matriz final:
+[
+    [7, 4, 1],
+    [8, 5, 2],
+    [9, 6, 3]
+]
+'''
+
+
+def girarMatriz(matriz: list, times: int = 1) -> list:
+    """
+    Rotaciona a matriz noventa graus, sentido horário 'n' vezes.
+    :param matriz: matriz solicitada.
+    :param times: qunatidade de vezes que a mateiz será rotaicionada.
+    :return: matriz rotacionada 90º sentido horário 'n' vezes.
+    """
+    if times % 4 == 0:
+        return matriz
+    elif times % 3 == 0 or times % 4 == 3:
+        times = 3
+    elif times % 2 == 0:
+        times = 2
+    else:
+        times = 1
+
+    for _ in range(times):
+        matriz_rotate: list = []
+        for i in range(len(matriz[1])):
+            matriz_rotate.append([])
+        for linha in range(len(matriz)):
+            last = matriz.pop(-1)
+            for i, item in enumerate(last):
+                matriz_rotate[i].append(item)
+        matriz = matriz_rotate
+    return matriz_rotate
+
+
+matriz_exemplo = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+matriz_rotacionada = girarMatriz(matriz_exemplo, 5)
+
+# Mostrando:
+for linha in matriz_rotacionada:
+    print(linha)
