eeae73c7e667a4c43fe0690837fa6d7e4b1e9f2a
Lucas Felix <85033278+CylexTTY@users.noreply.github.com>
2021-08-16 13:51:22 -0300

    Adicionado recursividade.
    Adicionado recursividade e retirado o "for" que controlava a quantidade de vezes.

--- girarMatriz.py
+++ girarMatriz.py
@@ -33,14 +33,12 @@ def girarMatriz(matriz: list, times: int = 1) -> list:
         case _:
             times = 1
 
-    for _ in range(times):
-        matriz_scope: list = [[] for _ in range(len(matriz[1]))]
-        for _ in range(len(matriz)):
-            last = matriz.pop(-1)
-            for i, item in enumerate(last):
-                matriz_scope[i].append(item)
-        matriz = matriz_scope
-    return matriz
+    matriz_scope: list = [[] for _ in range(len(matriz[1]))]
+    for j in range(len(matriz)-1, -1, -1):
+        last = matriz.pop(j)
+        for k, item in enumerate(last):
+            matriz_scope[k].append(item)
+    return girarMatriz(matriz_scope, times-1)
 
 
 matriz_exemplo = [
@@ -49,7 +47,7 @@ matriz_exemplo = [
     [7, 8, 9]
 ]
 
-matriz_rotacionada = girarMatriz(matriz_exemplo, 6)
+matriz_rotacionada = girarMatriz(matriz_exemplo, 3)
 # Mostrando:
 for linha in matriz_rotacionada:
     print(linha)
