4d58aa8736cdab259f3160df0fc0ac4516cdaed5
Lucas Meneses <lucasfelix3194@gmail.com>
2021-09-01 22:37:42 -0300

    Algoritmo de ordenação (Bubble Sort) em Python.

--- Algoritmos de ordenação/bubbleSort.py
+++ Algoritmos de ordenação/bubbleSort.py
@@ -1,11 +1,13 @@
 import random
 
+
 def bubbleSort(lista):
-    for _ in range(len(lista)-1):
-        for j in range(1, len(lista)):
-            if lista[j-1] > lista[j]:
-                lista[j-1], lista[j] = lista[j], lista[j-1]
+    for _ in range(len(lista) - 1):
+        for i in range(1, len(lista)):
+            if lista[i - 1] > lista[i]:
+                lista[i - 1], lista[i] = lista[i], lista[i - 1]
     return lista
+
 
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 random.shuffle(lista)
